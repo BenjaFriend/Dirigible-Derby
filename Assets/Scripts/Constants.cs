71e64cfe9b7d47688690230e42ff03d8349e56c1
Ben Hoffman <bah8892@g.rit.edu>
2018-02-22 16:01:22 -0500

    Basic input setup

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public static class RewiredInputActions
+    {
+        public const string JoinGame = "JoinGame";
+    }
+}
---
+++ Assets/Scripts/PressStartToJoin.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+/// <summary>
+/// This class will be in charge 
+/// </summary>
+public class PressStartToJoin : MonoBehaviour
+{
+    #region Fields
+    public int maxPlayerCount = 4;
+
+    public GameObject playerPrefab;
+
+    private int _currentPlayerCount = 0;
+    #endregion
+
+    private void Update()
+    {
+        // Watch for JoinGame action in each Player
+        for (int i = 0; i < ReInput.players.playerCount; i++)
+        {
+            if (ReInput.players.GetPlayer(i).GetButtonDown(Constants.RewiredInputActions.JoinGame))
+            {
+                CreateNextPlayer(i);
+            }
+        }
+    }
+
+    private void CreateNextPlayer(int rewiredPlayerId)
+    {
+        if(_currentPlayerCount >= maxPlayerCount)
+        {
+            return;
+        }
+
+        // instatiate
+        // give the rewired index
+
+        _currentPlayerCount++;
+    }
+
+}
