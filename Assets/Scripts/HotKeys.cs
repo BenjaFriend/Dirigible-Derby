b3eb854b9ea51e50e36fce54b9371761fee2249a
Ben Hoffman <bah8892@g.rit.edu>
2018-02-27 16:11:08 -0500

    Added the input testing as a playtest scene:

---
+++ Assets/Scripts/HotKeys.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotKeys : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log("Relaod!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
+	}
+}
--- Assets/Scripts/PressStartToJoin.cs
+++ Assets/Scripts/PressStartToJoin.cs
@@ -125,4 +125,9 @@ public class PressStartToJoin : MonoBehaviour
         }
     }
 
+    public void ResetSpawnRoom()
+    {
+        _activePlayers.Clear();
+    }
+
 }
