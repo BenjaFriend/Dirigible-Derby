d16f696119228cdcbd1f18274d51e9cb0ee22c12
rwaring <rowanwaring@gmail.com>
2018-03-05 17:02:34 -0500

    added more sound assets to project, re-arranged main menu layout

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    public AudioClip ProtoMusic;
+    public AudioClip Song;
 
     private PooledAudioSource _source;
 
     // Use this for initialization
     void Start()
     {
-        _source = AudioManager.Instance.Play(ProtoMusic, AudioPoolType.Music, Constants.Mixer.MixerGroups.Master.BackgroundMusic.Name, true, () => { _source = null; });
+        _source = AudioManager.Instance.Play(Song, AudioPoolType.Music, Constants.Mixer.MixerGroups.Master.BackgroundMusic.Name, true, () => { _source = null; });
     }
 
-    //private void OnDestroy()
-    //{
-    //    _source.Stop();
-    //}
+    private void ondestroy()
+    {
+        _source.Stop();
+    }
 
     // Update is called once per frame
     void Update()
