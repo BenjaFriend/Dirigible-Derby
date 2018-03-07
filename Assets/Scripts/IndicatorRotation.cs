12728f9f5e6cb91116a144fb802d2961c37c6708
rwaring <rowanwaring@gmail.com>
2018-03-07 15:16:26 -0500

    Player indicators no longer rotate with the player - z rotation locked to 0

---
+++ Assets/Scripts/IndicatorRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorRotation : MonoBehaviour {
+    
+    /// <summary>
+    /// Indicator Canvas
+    /// </summary>
+    public GameObject canvas;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        canvas.transform.rotation = Quaternion.Euler(Vector3.zero);
+	}
+}
