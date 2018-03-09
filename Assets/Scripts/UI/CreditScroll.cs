d8e94a1e4d85569a4b8be1bc3cc490e065f897d9
rwaring <rowanwaring@gmail.com>
2018-03-08 20:26:56 -0500

    Made CreditScroll more robust and dynamic

--- Assets/Scripts/UI/CreditScroll.cs
+++ Assets/Scripts/UI/CreditScroll.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CreditScroll : MonoBehaviour
@@ -8,14 +9,18 @@ public class CreditScroll : MonoBehaviour
     public GameObject Credits;
     public float Speed;
 
-    private const int _FINISH = 1350;
-
+    private float finish;
     private float actualSpeed;
 
     // Use this for initialization
     void Start()
     {
-        
+        var lastChild = Credits.transform.GetChild(Credits.transform.childCount - 1);
+        var childPosY = lastChild.position.y;
+        var childHeight = lastChild.GetComponent<RectTransform>().rect.height;
+        var canvasHeight = Credits.transform.parent.GetComponent<RectTransform>().rect.height;
+
+        finish = Math.Abs(childPosY) + childHeight + canvasHeight;
     }
 
     // Update is called once per frame
@@ -28,8 +33,7 @@ public class CreditScroll : MonoBehaviour
         var posY = Credits.GetComponent<RectTransform>().anchoredPosition.y;
 
         // scroll until the defined height
-        // TODO: Runtime grab defined height from all credit items?
-        if (posY < _FINISH)
+        if (posY < finish)
             Credits.GetComponent<RectTransform>().Translate(0, actualSpeed, 0);
 
         // Back to Main Menu
