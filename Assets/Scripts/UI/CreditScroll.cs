090f969a875f532501c70a75961cb16814173ab4
rwaring <rowanwaring@gmail.com>
2018-03-08 19:26:47 -0500

    Credits Scene created, scrolls some placeholder text - no way to get to it or exit back to anywhere yet

---
+++ Assets/Scripts/UI/CreditScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScroll : MonoBehaviour
+{
+
+    public GameObject Credits;
+    public float Speed;
+
+    private const int _FINISH = 1350;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var posY = Credits.GetComponent<RectTransform>().anchoredPosition.y;
+
+        if (posY < _FINISH)
+            Credits.GetComponent<RectTransform>().Translate(0, Speed, 0);
+    }
+}
