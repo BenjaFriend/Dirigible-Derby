e60bd0d14861ec48415f36ec48923456524bc634
Noah Ratcliff <me@nratcliff.com>
2018-03-29 17:48:54 -0400

    use scenecontroller and game manager scene change api for main menu

---
+++ Assets/Scripts/Scene Controllers/MainMenuController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MainMenuController : SceneController
+{
+    /// <summary>
+    /// A button that is to be selected by default.
+    /// </summary>
+    public UnityEngine.UI.Button FirstSelectButton;
+
+    public override void OnControllerActivated()
+    {
+        if(FirstSelectButton != null)
+        {
+            // Select the first button so that the input system knows what to do
+            FirstSelectButton.Select();
+        }        
+    }
+
+    /// <summary>
+    /// Load the main game scene
+    /// </summary>
+    public void LoadGame()
+    {
+        GameManager.Instance.ChangeScene(Constants.Scenes.PlayerLobby);
+    }
+
+    public void LoadCredits()
+    {
+        GameManager.Instance.ChangeScene(Constants.Scenes.Credits);
+    }
+
+    /// <summary>
+    /// Calls Application.quit
+    /// </summary>
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    public override void OnControllerDeactivated()
+    {
+    }
+}
